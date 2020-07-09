4118ec2e4c8a8ca50f83d330df182441847b36bf
nottak0409 <nottak0409@gmail.com>
2020-07-09 11:47:41 +0900

    add

--- contact/confirm.php
+++ contact/confirm.php
@@ -72,19 +72,16 @@ EOM;
 <meta charset="UTF-8">
 <title>お問い合わせフォーム</title>
 <link rel="stylesheet" href="../lib/style.css">
-</head>
 <body>
 <div><h1>Company Name</h1></div>
 <div><h2>お問い合わせ</h2></div>
 <div>
     <form action="confirm.php" method="post">
             <input type="hidden" name="name" value="<?php echo $name; ?>">
-            <input type="hidden" name="furigana" value="<?php echo $furigana; ?>">
             <input type="hidden" name="email" value="<?php echo $email; ?>">
             <input type="hidden" name="tel" value="<?php echo $tel; ?>">
-            <input type="hidden" name="sex" value="<?php echo $sex; ?>">
-            <input type="hidden" name="item" value="<?php echo $item; ?>">
-            <input type="hidden" name="content" value="<?php echo $content; ?>">
+            <input type="hidden" name="subject" value="<?php echo $subhect; ?>">
+            <input type="hidden" name="contact" value="<?php echo $contact; ?>">
             <h1 class="contact-title">お問い合わせ 内容確認</h1>
             <p>お問い合わせ内容はこちらで宜しいでしょうか？<br>よろしければ「送信する」ボタンを押して下さい。</p>
             <div>
