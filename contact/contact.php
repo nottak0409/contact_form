a01eb8149a4c0b469d416cc9817e689cbfb8d20f
nottak0409 <nottak0409@gmail.com>
2020-07-10 12:02:04 +0900

    form-change

--- contact/contact.php
+++ contact/contact.php
@@ -41,37 +41,37 @@ foreach(array('default', 'error', 'warning') as $key) {
         <label for="name">お名前（30文字以内)(必須)
           <span class="error text-danger"><?php if (isset ($_SESSION["error_name"] ) ) echo h($_SESSION["error_name"]); ?></span>
         </label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="氏名" value="<?php echo h($name) ?>">
+        <input type="text" class="form-control" id="name" name="name" placeholder="氏名" value="<?php echo h($_SESSION["name"]) ?>">
       </div>
       <div class="form-group">
         <label for="email">メールアドレス(必須)
           <span class="error text-danger"><?php if (isset ($_SESSION["error_email"] ) ) echo h($_SESSION["error_email"]); ?></span>
         </label>
-        <input type="text" class="form-control" id="email" name="email" placeholder="メールアドレス" value="<?php echo h($email) ?>">
+        <input type="text" class="form-control" id="email" name="email" placeholder="メールアドレス" value="<?php echo h($_SESSION["email"]) ?>">
       </div>
       <div class="form-group">
         <label for="email_check text-danger">メールアドレス(確認)
           <span class="error text-danger"><?php if (isset ($_SESSION["error_email_check"] ) ) echo h($_SESSION["error_email_check"]); ?></span>
         </label>
-        <input type="text" class="form-control" id="email_check" name="email_check" placeholder="メールアドレス(確認)" value="<?php echo h($email_check) ?>">
+        <input type="text" class="form-control" id="email_check" name="email_check" placeholder="メールアドレス(確認)" value="<?php echo h($_SESSION["email_check"]) ?>">
       </div>
       <div class="form-group">
         <label for="tel">お電話番号(半角英数字)(必須)
           <span class="error text-danger"><?php if (isset ($_SESSION["error_tel"] ) ) echo h($_SESSION["error_tel"]); ?></span>
         </label>
-        <input type="text" class="form-control" id="tel" name="tel" placeholder="電話番号" value="<?php echo h($tel) ?>">
+        <input type="text" class="form-control" id="tel" name="tel" placeholder="電話番号" value="<?php echo h($_SESSION["tel"]) ?>">
       </div>
       <div class="form-group">
         <label for="subject">件名(必須)
           <span class="error text-danger"><?php if (isset ($_SESSION["error_subject"])) echo h($_SESSION["error_subject"]); ?></span>
         </label>
-        <input type="text" class="form-control" id="subject" name="subject" placeholder="件名" value="<?php echo h($subject) ?>">
+        <input type="text" class="form-control" id="subject" name="subject" placeholder="件名" value="<?php echo h($_SESSION["subject"]) ?>">
       </div>
       <div class="form-group">
         <label for="contact">お問い合わせ内容(必須)
           <span class="error text-danger"><?php if (isset ($_SESSION["error_contact"] ) ) echo h($_SESSION["error_contact"]); ?></span>
         </label>
-        <textarea class="form-control" id="contact" name="contact" placeholder="お問い合わせ内容（1000文字まで)(必須)" row=3><?= h($contact); ?></textarea>
+        <textarea class="form-control" id="contact" name="contact" placeholder="お問い合わせ内容（1000文字まで)(必須)" row=3><?= h($_SESSION["contact"]); ?></textarea>
       </div>
       <input type="hidden" name="ticket" value="<?= h($ticket); ?>">
       <button name="submitted" type="submit" class="btn btn-primary">送信</button>
