11ce91422569c04dd25c5e5e327a5bdfa55dd1c7
nottak0409 <nottak0409@gmail.com>
2020-07-13 13:12:36 +0900

    responsive

--- contact/complete.php
+++ contact/complete.php
@@ -27,6 +27,7 @@ if(isset($_POST['submit'])) {
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>お問い合わせフォーム</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <link rel="stylesheet" href="./stylesheet.css">
